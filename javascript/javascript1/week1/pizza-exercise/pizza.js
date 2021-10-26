d743cfdc6bf771cfe21bf5a01947964747579aaa
Asma Fassy <asmafassy@gmail.com>
2021-10-26 17:59:16 +0200

    Week01 JavaScript exercises

---
+++ javascript/javascript1/week1/pizza-exercise/pizza.js
@@ -0,0 +1,13 @@
+console.log("I love pizza");
+
+const myFavPizza = 'Pan Pizza';
+const costOfPizza = 65
+console.log(`New pizza order:, ${myFavPizza}. The price of the pizza is : ${costOfPizza}`)
+
+const noPizza = 18;
+let totalPrice = noPizza * costOfPizza
+let familyPizza = true;
+
+if(familyPizza){
+    console.log(`New pizza order: ${noPizza} ${familyPizza} ${myFavPizza}. Total cost for the order is : ${totalPrice}`)
+}
---
+++ javascript/javascript1/week1/week1 exercises/script.js
@@ -0,0 +1,21 @@
+/* console.log('Hello World')
+
+let myName = 'Default';
+myName = 'John'
+
+let myAge = 35;
+ 
+// add your code here
+
+
+// DO NOT change the code below
+console.log(`In 20 years, ${myName} will be ${myAge + 20}`)
+*/
+
+
+let phWage = 55;
+
+console.log(`In 24 hours my wages will be, ${phWage * 24}`)
+
+const myName = 'Asma'
+console.log
