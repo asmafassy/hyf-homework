961a96ad38ba90966c5fa00f3bb13103e5f6c078
Asma Fassy <asmafassy@gmail.com>
2022-02-17 21:39:24 +0100

    added part of homework (hyf-bay)

--- javascript/javascript2/week2/03-hyfBay/script.js
+++ javascript/javascript2/week2/03-hyfBay/script.js
@@ -1,44 +1,48 @@
-console.log(5)
+console.log(5);
 
 console.log("Script loaded");
 
 let products = getAvailableProducts();
-const ul = document.querySelector("ul");
-console.log(ul)
-console.log(products);
+let filteredProducts = [];
+
+const ul = document.querySelector("#searchResult");
 
 function getRow(title, price, rate) {
-  return`<li>
+  return `<li>
         <h2> ${title} </h2>
         <span> price ${price} </span> <br/>
         <span> rating ${rate} </span>
     </li>`;
 }
-console.log(main);
+
+function filterProducts(text) {
+  filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(text.toLowerCase())
+  );
+  renderProducts();
+}
 
 function renderProducts() {
   let rows = "";
-  for (let i = 0; i < products.length; i++) {
-      let product = products[i]
-   rows += getRow(product.name, product.price, product.rating);
+  for (let i = 0; i < filteredProducts.length; i++) {
+    let product = filteredProducts[i];
+    rows += getRow(product.name, product.price, product.rating);
   }
-  console.log(rows)
-  ul.innerHTML = rows
+  ul.innerHTML = rows;
 }
-renderProducts();
 
+document.getElementById("search").addEventListener("keyup", (event) => {
+  filterProducts(event.target.value);
+});
 
-
-function filterProducts(text){
-
-    products.filter(product => {
-        console.log(product.name.includes(text))
-        if(product.name.includes(text)){
-            return product;
-        }
-     });
-     renderProducts()
-
+//Filter products based on max price
+function filterPrice(num) {
+  filteredProducts = products.filter((product) => product.price < num);
+  renderProducts();
 }
-filterProducts('Used')
 
+document.getElementById("search2").addEventListener("keyup", (event) => {
+  console.log(event.target.value);
+
+  filterPrice(event.target.value);
+});
