c2b12332cdf2256f3c387491e68a66f0d5d6e91b
Asma Fassy <asmafassy@gmail.com>
2022-03-08 21:34:19 +0100

    nodejs-week2

--- nodejs/week2/homework/meal-sharing/src/backend/api/meals-router.js
+++ nodejs/week2/homework/meal-sharing/src/backend/api/meals-router.js
@@ -3,14 +3,17 @@ const router = express.Router();
 
 const meals = require("./../data/meals.json");
 
-router.get("/", async (request, response) => {
-  try {
-    console.log(meals);
-    console.log("in /api/meals");
-  } catch (error) {
-    throw error;
-  }
-});
 
+// router.get("/", async (request, response) => {
+//   try {
+//     //console.log(meals);
+//     console.log("in /api/meals");
+//   } catch (error) {
+//     throw error;
+//   }
+// });
+
+
+ 
 
 module.exports = router;
--- nodejs/week2/homework/meal-sharing/src/backend/app.js
+++ nodejs/week2/homework/meal-sharing/src/backend/app.js
@@ -1,23 +1,81 @@
 const express = require("express");
 const app = express();
 
-const meals = require("./data/meals.json");
-
 const mealsRouter = require("./api/meals-router");
-
-// app.use binds middleware to your application. You can give app.use a path and router. The mini router will take care of all requests with the path
 app.use("/api/meals", mealsRouter);
 
-mealsRouter.get('/:id', (req, res) => {
-const id = req.params.id
-const mealid = meals.filter(meal => {
-    meals.id;
-    if (meals.id === req.params.id )
-    res.send(meals);
-    // console.log(meals);
-})
- 
-})
+const meals = require("./data/meals.json");
+const reservations = require("./data/reservations.json");
 
+const reservationsRouter = require("./api/reservations-router");
+app.use("/api/reservations", reservationsRouter);
+
+const reviewsRouter = require("./api/reviews-router");
+const reviews = require("./data/reviews.json");
+app.use("/api/reviews", reviewsRouter);
+
+// app.use binds middleware to your application. You can give app.use a path and router. The mini router will take care of all requests with the path
+mealsRouter.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const filteredMeal = meals.filter((meal) => meal.id === parseInt(id));
+  res.send(filteredMeal);
+});
+
+//Respond with the json for all the meals
+// mealsRouter.get("/", (req, res) => {
+//   res.send(meals);
+// });
+
+//Respond with the json for the reservation with the corresponding id *
+reservationsRouter.get("/:id", (req, res) => {
+  const filteredReservation = reservations.filter(
+    (reservation) => reservation.id === parseInt(req.params.id)
+  );
+  res.send(filteredReservation);
+});
+
+//Respond with the json for all reservations *
+reservationsRouter.get("/", (req, res) => {
+  res.send(reservations);
+});
+
+// Respond with the json for the review with the corresponding id *
+reviewsRouter.get("/:id", (req, res) => {
+  const filteredreview = reviews.filter(
+    (review) => review.id === parseInt(req.params.id)
+  );
+  res.send(filteredreview);
+});
+
+// Respond with the json for all reviews *
+reviewsRouter.get("/", (req, res) => {
+  res.send(reviews);
+});
+
+//Get meals that has a price smaller than maxPrice
+mealsRouter.get("/", (req, res) => {
+  let data = meals;
+  console.log("String for testing");
+console.log(req.query);
+
+  if ("maxPrice" in req.query) {
+    const maxPrice = Number(req.query.maxPrice);
+    data = data.filter((meal) => meal.price <= maxPrice);
+    console.log(`data : ${data}`);
+    console.log(`max price  : ${maxPrice}`);
+  }
+  
+  if("title" in req.query){
+      const title = req.query.title;
+      data = data.filter(meal => meal.title.match(title))
+  }
+  if("createdAfter" in req.query){
+      const createdAfter = req.query.createdAfter;
+      data = data.filter(meal => meal.createdAt <= createdAfter);
+  }
+
+
+  res.send(data);
+});
 
 module.exports = app;
